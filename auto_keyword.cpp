2b3a109e87c6dd19332a99fbafb303f53c3ab259
aasishdarrel <aasishdairel@gmail.com>
2025-08-23 00:33:48 +0530

    updated files

--- auto_keyword.cpp
+++ auto_keyword.cpp
@@ -2,11 +2,13 @@
 #include<typeinfo>
 
 int main()
-{
+{   // auto keyword use to improve work flow the doesn"t wan explicitly want to declare the datatype it auto detects it
     auto aasish = "he is the best";
     auto dairel = "he will learn cpp";
     std::cout<<aasish<<std::endl;
     std::cout<<dairel<<std::endl;
+    /* type information is used to identify the which type(which datatype) of a paricular variable delcared
+     and .name() shows the short name of datatype eg integer = i , float = f et....... */
     std::cout<<typeid(aasish).name()<<" "<< typeid(dairel).name()<<std::endl;
 return 0;
 }
